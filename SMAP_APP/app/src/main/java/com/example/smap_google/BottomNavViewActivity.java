61c67d1674b8dfad47e66e35ff92573e5e5dcc9b
16chperfect <sanghyun040714@gmail.com>
2020-09-04 18:20:56 +0900

    @Override dhfb

--- SMAP_APP/app/src/main/java/com/example/smap_google/BottomNavViewActivity.java
+++ SMAP_APP/app/src/main/java/com/example/smap_google/BottomNavViewActivity.java
@@ -1,9 +1,12 @@
 package com.example.smap_google;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
---
+++ SMAP_APP/app/src/main/java/com/example/smap_google/LocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.smap_google;
+
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+public class LocationActivity {
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    @Override
+        protected void onCreate(Bundle savedInstanceState){
+        fusedLocationProviderClient
+    }
+}
