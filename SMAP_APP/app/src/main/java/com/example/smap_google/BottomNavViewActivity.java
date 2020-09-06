12a1aa38eef011fd7ce17057074ce89e367a1170
16chperfect <sanghyun040714@gmail.com>
2020-09-06 19:39:59 +0900

    바텀네비게이션+로케이션

--- SMAP_APP/app/src/main/java/com/example/smap_google/BottomNavViewActivity.java
+++ SMAP_APP/app/src/main/java/com/example/smap_google/BottomNavViewActivity.java
@@ -2,20 +2,30 @@ package com.example.smap_google;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class BottomNavViewActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -87,18 +97,105 @@ public class BottomNavViewActivity extends AppCompatActivity implements OnMapRea
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
-        mMap = googleMap;
+//        mMap = googleMap;
+//
+//        LatLng SEOUL = new LatLng(37.56, 126.97);
+//
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(SEOUL);
+//        markerOptions.title("서울");
+//        markerOptions.snippet("한국의 수도");
+//        mMap.addMarker(markerOptions);
+//
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        map = googleMap;
+        getMyLocation();
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+    }
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
+    private  static  final LatLng DEFAULT_LOCATION = new LatLng(37.56641923090,126.9778741551);
+    private  static  final  int DEFAULT_ZOOM = 15;
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+    private static final long INTERVAL_TIME = 5000;
+    private static  final  long FASTEST_INTERVAL_TIME = 2000;
+
+    private GoogleMap map;
+    private MapView mapView;
+    private Location lastknowLocation;
+
+    private void getMyLocation(){
+        if(ActivityCompat.checkSelfPermission(BottomNavViewActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(BottomNavViewActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+
+        FusedLocationProviderClient fusedLocationProviderClient = new FusedLocationProviderClient(this);
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>(){
+
+
+            @Override
+            public void onSuccess(Location location) {
+                lastknowLocation = location;
+
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(lastknowLocation.getLatitude(),
+                                lastknowLocation.getLongitude()),
+                        DEFAULT_ZOOM
+                ));
+                updateMyLocation();
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(ActivityCompat.checkSelfPermission(BottomNavViewActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(BottomNavViewActivity.this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION,
+                        DEFAULT_ZOOM));
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        });
+    }
+
+    private void updateMyLocation() {
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(INTERVAL_TIME);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL_TIME);
+
+        FusedLocationProviderClient fusedLocationProviderClient = new FusedLocationProviderClient(this);
+
+        fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest, new LocationCallback(){
+
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        Location location = locationResult.getLastLocation();
+
+                        map.moveCamera(CameraUpdateFactory.newLatLng(
+                                new LatLng(location.getLatitude(), location.getLongitude())));
+                    }
+
+                },null);
 
     }
 
