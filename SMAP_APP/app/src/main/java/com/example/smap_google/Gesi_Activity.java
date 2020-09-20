7871b635361cb696125f17b93989ae446e508c65
16chperfect <sanghyun040714@gmail.com>
2020-09-21 00:05:15 +0900

    파이어베이스 gesi_activity

--- SMAP_APP/app/src/main/java/com/example/smap_google/Gesi_Activity.java
+++ SMAP_APP/app/src/main/java/com/example/smap_google/Gesi_Activity.java
@@ -1,50 +1,73 @@
 package com.example.smap_google;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
+import static java.security.AccessController.getContext;
+
 public class Gesi_Activity extends AppCompatActivity {
-    private ImageButton uploadButton;
+    private Button Button;
     private ArrayList<gesipan> arrayList;
-    private  Maindapter maindapter;
+    private Maindapter maindapter;
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-      super.onCreate(savedInstanceState);
-        setContentView(R.layout.gesipan);
 
-        recyclerView = (RecyclerView)findViewById(R.id.rv);
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gesi);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
         linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
 
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("snapshot"); // DB 테이블 연결
 
         arrayList = new ArrayList<>();
 
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    gesipan cafeList = snapshot.getValue(gesipan.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                    arrayList.add(cafeList); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                maindapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("Fraglike", String.valueOf(error.toException()));
+            }
+
+
+        });
         maindapter = new Maindapter(arrayList);
         recyclerView.setAdapter(maindapter);
 
-        uploadButton = (ImageButton) findViewById(R.id.btn_upload);
-        uploadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gesipan gesi = new gesipan(R.drawable.ic_launcher_background,".getText().toString()");
-                arrayList.add(gesi);
-                maindapter.notifyDataSetChanged();
-            }
-        });
-
-
     }
-
-
 }
+
